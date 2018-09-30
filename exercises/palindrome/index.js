57de157027f27a307d0ae894452469d80b5b1c33
SparksFullStack <csparks@antioch.edu>
2018-09-30 15:55:11 -0700

    palindrome solutions finished

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,25 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    // * conversion + ternary solution (Grider's solution)
+    // const revStr = str.split('').reverse().join('');
+    // return str === revStr;
+
+    // * no reverse helper method
+    // let newStr = ''
+    // for (let i = str.length - 1; i >= 0; i--) newStr += str[i];
+    // if (newStr === str) return true;
+    // else return false;
+
+    // * one liner
+    // return str.split('').reverse().join('') === str ? true : false;
+
+    // array.every
+    const arr = str.split('');
+    return arr.every((char, i) => {
+        return char === str[str.length - i - 1];
+    })
+}
 
 module.exports = palindrome;
